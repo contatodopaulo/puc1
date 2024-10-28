3d23e279d124c1585e80fdff0c983b6a1de84fdf
contatodopaulo <contatodopaulo@gmail.com>
2024-10-28 16:44:43 -0300

    port

--- app/app.py
+++ app/app.py
@@ -120,4 +120,4 @@ def signup():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2053, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
